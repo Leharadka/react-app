35a08f7ef88acd0545f4276ca05507f20947514e
Aleksei <alex.porfirov@gmail.com>
2025-05-21 13:51:29 +0300

    добавил стейт кнопке

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,21 @@
 import "./Button.css";
+import { useState } from "react";
+
 function Button() {
-  return <button className='button accent'>Сохранить</button>;
+  // let text = "Сохранить";
+  const [text, setText] = useState("Сохранить");
+
+  const cliked = () => {
+    setText("Закрыть");
+    text = "Закрыть";
+    console.log("Hello!");
+  };
+
+  return (
+    <button onClick={cliked} className="button accent">
+      {text}
+    </button>
+  );
 }
 
 export default Button;

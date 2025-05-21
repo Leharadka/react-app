281717cb1f55a37ef64eebc388befce5408faa94
Aleksei <alex.porfirov@gmail.com>
2025-05-21 13:51:05 +0300

    Перезалил проект

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import Button from './components/Button/Button'
+import CardButton from './components/CardButton/CardButton'
+import JournalItem from './components/JournalItem/JournalItem'
+
+function App() {
+  const data = [
+    {
+      title: 'Подготовка к обновлению курсов',
+      text: 'Горные походы открывают удивительные природные ландшафты',
+      date: new Date()
+    },
+     {
+      title: 'Поход в горы',
+      text: 'Думал, что очень много времени',
+      date: new Date()
+    }
+  ]
+
+  // return React.createElement('div', {}, 'Проект');
+  
+  return (
+    <>
+      <h1>Какой-то текст</h1>
+      <p>Заголовок</p>
+      <Button/>
+      <CardButton>
+        Новое воспоминание
+      </CardButton>
+      <CardButton>
+      <JournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      </CardButton>
+      <CardButton>
+      <JournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      />
+      </CardButton>
+    </>
+  )
+}
+
+export default App

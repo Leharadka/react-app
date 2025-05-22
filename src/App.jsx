722f27d1643f6806cf267fd806969cb127a4f66f
Aleksei <alex.porfirov@gmail.com>
2025-05-22 20:59:11 +0300

    Сверстал и стилизовал левую панель с кнопками

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,56 @@
-import './App.css'
-import Button from './components/Button/Button'
-import CardButton from './components/CardButton/CardButton'
-import JournalItem from './components/JournalItem/JournalItem'
+import "./App.css";
+import Button from "./components/Button/Button";
+import CardButton from "./components/CardButton/CardButton";
+import Header from "./components/Header/Header";
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import JournalItem from "./components/JournalItem/JournalItem";
+import JournalList from "./components/JournalList/JournalList";
+import Body from "./layouts/Body/Body";
+import LeftPanel from "./layouts/LeftPanel/LeftPanel";
 
 function App() {
   const data = [
     {
-      title: 'Подготовка к обновлению курсов',
-      text: 'Горные походы открывают удивительные природные ландшафты',
-      date: new Date()
+      title: "Подготовка к обновлению курсов",
+      text: "Горные походы открывают удивительные природные ландшафты",
+      date: new Date(),
     },
-     {
-      title: 'Поход в горы',
-      text: 'Думал, что очень много времени',
-      date: new Date()
-    }
-  ]
+    {
+      title: "Поход в горы",
+      text: "Думал, что очень много времени",
+      date: new Date(),
+    },
+  ];
 
   // return React.createElement('div', {}, 'Проект');
-  
+
   return (
-    <>
-      <h1>Какой-то текст</h1>
-      <p>Заголовок</p>
-      <Button/>
-      <CardButton>
-        Новое воспоминание
-      </CardButton>
-      <CardButton>
-      <JournalItem
-        title={data[0].title}
-        text={data[0].text}
-        date={data[0].date}
-      />
-      </CardButton>
-      <CardButton>
-      <JournalItem
-        title={data[1].title}
-        text={data[1].text}
-        date={data[1].date}
-      />
-      </CardButton>
-    </>
-  )
+    <div className="app">
+      <LeftPanel>
+        <Header/>
+        <JournalAddButton/>
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              text={data[0].text}
+              date={data[0].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              text={data[1].text}
+              date={data[1].date}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>
+        Body
+      </Body>
+    </div>
+  );
 }
 
-export default App
+export default App;

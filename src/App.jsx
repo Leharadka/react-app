86f3386af3108539a139a923122d0b9db10d92f3
Aleksei <alex.porfirov@gmail.com>
2025-05-25 13:58:45 +0300

    Сделал добавление новой записи + сортировка

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,71 @@
-
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import CardButton from "./components/CardButton/CardButton";
 import Header from "./components/Header/Header";
-import JournalAddButton from './components/JournalAddButton/JournalAddButton';
-import JournalForm from './components/JournalForm/JournalForm';
+import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
+import JournalForm from "./components/JournalForm/JournalForm";
 import JournalItem from "./components/JournalItem/JournalItem";
 import JournalList from "./components/JournalList/JournalList";
 import Body from "./layouts/Body/Body";
 import LeftPanel from "./layouts/LeftPanel/LeftPanel";
 
-function App() {
-  const data = [
-    {
-      title: "Подготовка к обновлению курсов",
-      text: "Горные походы открывают удивительные природные ландшафты",
-      date: new Date(),
-    },
-    {
-      title: "Поход в горы",
-      text: "Думал, что очень много времени",
-      date: new Date(),
-    },
-  ];
+const INITIAL_DATA = [
+  {
+    id: 1,
+    title: "Подготовка к обновлению курсов",
+    text: "Горные походы открывают удивительные природные ландшафты",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Поход в горы",
+    text: "Думал, что очень много времени",
+    date: new Date(),
+  },
+];
 
- 
-  // return React.createElement('div', {}, 'Проект');
+function App() {
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = (item) => {
+    setItems((oldItems) => [
+      ...oldItems,
+      {
+        text: item.text,
+        title: item.title,
+        date: new Date(item.date),
+        id: Math.max(...oldItems.map(i => i.id)) + 1
+      },
+    ]);
+  };
+
+  const sortItems = (a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    } else {
+      return -1
+    }        
+  };
 
   return (
     <div className="app">
       <LeftPanel>
-        <Header/>
-        <JournalAddButton/>
+        <Header />
+        <JournalAddButton />
         <JournalList>
-          <CardButton>
-            <JournalItem
-              title={data[0].title}
-              text={data[0].text}
-              date={data[0].date}
-            />
-          </CardButton>
-          <CardButton>
-            <JournalItem
-              title={data[1].title}
-              text={data[1].text}
-              date={data[1].date}
-            />
-          </CardButton>
+          {items.sort(sortItems).map((el) => (
+            <CardButton key={el.id}>
+              <JournalItem 
+              title={el.title} 
+              text={el.text} 
+              date={el.date} />
+            </CardButton>
+          ))}
         </JournalList>
       </LeftPanel>
       <Body>
-        <JournalForm/>
+        <JournalForm onSubmit={addItem} />
       </Body>
     </div>
   );
